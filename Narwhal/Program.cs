6a055190cb032bd75bfbca1aa454912bd914705e
kirinnee <kirinnee97@gmail.com>
2019-10-10 09:24:39 +0800

    Initial Commit~

---
+++ Narwhal/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace main
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Narwhal:\n" +
+                    "\n" +
+                    "A .NET Core CLI to save and load docker volumes to tarballs" +
+                    "\n" +
+                    "Use narwhal --help to see list of commands\n");
+                return;
+            }
+            if(args.Length == 1)
+            {
+                if(args[0] == "--help")
+                {
+                    Console.WriteLine("narwhal <message> : prints message in caps");
+                }
+                else
+                {
+                    Console.WriteLine(args[0].ToUpper());
+                }
+            }
+        }
+
+        public static string ReverseString(string x)
+        {
+            return string.Join("", x.ToCharArray().Reverse());
+        }
+    }
+}
